b4a19c825f93602852e4642ce41f4463e5c8e60e
Dan Neame <danneame@gmail.com>
2018-03-02 18:11:29 +0000

    Add confirmation to 'New Combat' button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <header>
-      <a @click="newCombat">New Combat</a>
+      <a @click="confirmNewCombat = true">New Combat</a>
       <a @click="nextPlayer">Next</a>
       <add-player></add-player>
     </header>
@@ -11,6 +11,11 @@
       </div>
       <player v-for="player in players" :key="player.id" v-bind:player="player"></player>
     </div>
+    <confirmation-modal v-if="confirmNewCombat"
+      message="This will delete your current combat."
+      @confirm="confirmNewCombat = false;newCombat()"
+      @cancel="confirmNewCombat = false"
+    ></confirmation-modal>
   </div>
 </template>
 
@@ -18,11 +23,13 @@
 import { mapGetters, mapActions } from 'vuex'
 import AddPlayer from './components/players/AddPlayer'
 import Player from './components/players/Player'
+import ConfirmationModal from './components/utility/ConfirmationModal'
 
 export default {
   components: {
     AddPlayer,
-    Player
+    Player,
+    ConfirmationModal
   },
   computed: mapGetters({
     players: 'players',
@@ -31,7 +38,12 @@ export default {
   methods: mapActions([
     'nextPlayer',
     'newCombat'
-  ])
+  ]),
+  data () {
+    return {
+      confirmNewCombat: false
+    }
+  }
 }
 </script>
 
---
+++ src/components/utility/ConfirmationModal.vue
@@ -0,0 +1,41 @@
+<template>
+  <modal>
+    <h3 slot="header">Confirm</h3>
+    <div slot="body">
+      <p>
+        {{message}}
+      </p>
+      <p>
+        <button @click="$emit('confirm')">Confirm</button>
+        <button @click="$emit('cancel')">Cancel</button>
+      </p>
+    </div>
+  </modal>
+</template>
+
+<script>
+import Modal from './Modal'
+
+export default {
+  components: {
+    Modal
+  },
+  props: ['message'],
+  methods: {
+    onKeyPress (e) {
+      if (e.key === 'Escape') {
+        this.$emit('cancel')
+      }
+      if (e.key === 'Enter') {
+        this.$emit('confirm')
+      }
+    }
+  },
+  created () {
+    window.addEventListener('keyup', this.onKeyPress)
+  },
+  beforeDestroy () {
+    window.removeEventListener('keyup', this.onKeyPress)
+  }
+}
+</script>
