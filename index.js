025cd48b25fb011cd2ca7ce4726290802545e696
Dan Neame <danneame@gmail.com>
2018-03-03 14:27:16 +0000

    Customise menu, remove devtools

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, protocol } = require('electron')
+const { app, BrowserWindow, protocol, Menu } = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -18,11 +18,26 @@ function createWindow () {
     slashes: true
   }))
 
-  win.webContents.openDevTools()
-
   win.on('closed', () => {
     win = null
   })
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate([{
+    label: "Application",
+    submenu: [
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+  ]))
 
   protocol.interceptFileProtocol('file', (request, callback) => {
     let url = request.url.substr(PROTOCOL.length + 1)
